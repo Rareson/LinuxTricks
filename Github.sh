303cea4a748766c876cd9c64a8f507de1ecad771
Rareson <g1114128525@gmail.com>
2014-09-25 12:22:17 +0800

    about large files.

--- Github.sh
+++ Github.sh
@@ -6,7 +6,7 @@ git clone git@github.com:xxx/xxx.git # clone with write and read permissions.
 git pull git@github.com:xxx/xxx.git 
 git push # first cd to the repository and use this to push your updates.
 
-cd /home/domicor/LinuxTricks
+cd /home/domicor/LammpsRelated
 git push
 
 
@@ -27,6 +27,13 @@ http://schacon.github.io/git/user-manual.html
 
 Github help
 https://help.github.com/
+
+About large file and releases: 
+https://help.github.com/articles/what-is-my-disk-quota
+https://help.github.com/articles/creating-releases
+https://help.github.com/articles/linking-to-releases
+https://help.github.com/articles/distributing-large-binaries
+
 
 Chinese: http://blog.csdn.net/five3/article/details/8904635
 
