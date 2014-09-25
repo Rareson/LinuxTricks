c48204adcadf8b72c5b9b57be3ad5c5887dc264f
Rareson <g1114128525@gmail.com>
2014-09-25 10:25:15 +0800

    added github and lammps

---
+++ Github.sh
@@ -0,0 +1,33 @@
+ssh-keygen # to generate the SSH key
+# then add the key to your Github account.
+ssh -T git@github.com
+
+git clone git@github.com:xxx/xxx.git # clone with write and read permissions.
+git pull git@github.com:xxx/xxx.git 
+git push # first cd to the repository and use this to push your updates.
+
+cd /home/domicor/LinuxTricks
+git push
+
+
+
+
+
+
+
+
+
+
+
+
+References:
+
+Git User’s Manual (for version 1.5.3 or newer)
+http://schacon.github.io/git/user-manual.html
+
+Github help
+https://help.github.com/
+
+Chinese: http://blog.csdn.net/five3/article/details/8904635
+
+
